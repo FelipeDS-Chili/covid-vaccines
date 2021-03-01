c05e8bf5fd1cf45965dbb7acff9e730de2af1b81
felipe inostroza <falberto.inostroza@gmail.com>
2021-03-01 03:32:13 -0300

    commit

--- app_streamlit.py
+++ app_streamlit.py
@@ -18,31 +18,54 @@ data = pd.read_csv(url, error_bad_lines=False).drop(columns = ['iso_code'])
 def get_bar_chart_data():
     print('get_bar_chart_data called')
 
-    top_20_vaccines = get_df(data).sort_values('total_vaccinations', ascending = False).head(20)
+    top_20_vaccines = get_df(data).sort_values('total_vaccinations', ascending = False).head(30)
 
     return top_20_vaccines[['country', 'vac_per_100000']].sort_values('vac_per_100000', ascending = True)
-#.set_index('country') antes de sort
+
+
+@st.cache
+def get_bar_chart_data_total():
+    print('get_bar_chart_data called')
+
+    top_20_vaccines = get_df(data).sort_values('total_vaccinations', ascending = False).head(30)
+
+    return top_20_vaccines[['country', 'total_vaccinations']]
+
+
+st.header('Covid Vaccination Progress')
+
+
+st.subheader('Total Vaccinations')
+
+chart_total = get_bar_chart_data_total()
+
+brush = alt.selection(type='interval', encodings=['y'])
+
+st.altair_chart(alt.Chart(chart_total, width = 800, height = 800).mark_bar().encode(
+    x=alt.X('total_vaccinations'),
+    y=alt.Y('country', sort ='-x'
+        ),color = "country:N"
+).add_selection(
+    brush
+).interactive().transform_filter(
+    brush ).properties(title="Total Vaccinations by Country").resolve_scale(color='shared'))
 
 
 
 
-'Starting a long computation...'
-
-
+st.subheader('Vaccinations per inhabitants')
 
 chart_data = get_bar_chart_data()
-
-st.write('COVID19')
-st.bar_chart(chart_data, width = 0, height = 0)
 
 brush = alt.selection(type='interval', encodings=['y'])
 
 st.altair_chart(alt.Chart(chart_data, width = 800, height = 800).mark_bar().encode(
     x=alt.X('vac_per_100000'),
-    y=alt.Y('country', sort ='-x'),
+    y=alt.Y('country', sort ='-x'
+        ),color = "country:N"
 ).add_selection(
     brush
 ).interactive().transform_filter(
-    brush ).properties(title="Vaccinations per 100000 inhabitants"))
+    brush ).properties(title="Vaccinations per 100.000 inhabitants").resolve_scale(color='shared'))
 
 
