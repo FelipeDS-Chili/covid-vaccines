1f6df1b703acdba3100064d22899c268fb525e72
felipe inostroza <falberto.inostroza@gmail.com>
2021-03-01 15:08:15 -0300

    solved

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='toolbox',
+setup(name='covidvaccines',
       version="1.0",
       description="Project Description",
       packages=find_packages(),
@@ -13,5 +13,5 @@ setup(name='toolbox',
       install_requires=requirements,
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/toolbox-run'],
+      scripts=['scripts/covid-vaccines-run'],
       zip_safe=False)
