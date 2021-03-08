6661740adbc86169272183437f582b4957b67cdb
felipe inostroza <falberto.inostroza@gmail.com>
2021-03-08 00:42:57 -0300

    last one

--- app_streamlit.py
+++ app_streamlit.py
@@ -8,6 +8,7 @@ import os
 import json
 import time
 import altair as alt
+import base64
 
 #title = st.text_input('Nombre del Anime', 'naruto')
 
@@ -20,7 +21,7 @@ def len_data():
     print(' Calculando largo de data ...')
     return len(data)
 
-@st.cache
+@st.cache(suppress_st_warning = True)
 def get_bar_chart_data():
     print('get_bar_chart_data called')
 
@@ -31,7 +32,7 @@ def get_bar_chart_data():
 
 
 
-@st.cache
+@st.cache(suppress_st_warning = True)
 def get_bar_chart_data_total():
     print('get_bar_chart_data called')
 
@@ -83,4 +84,31 @@ st.altair_chart(alt.Chart(chart_data, width = 800, height = 800).mark_bar().enco
 ).interactive().properties(title="Top 30: Vaccinations per 100.000 inhabitants").resolve_scale(color='shared'))
 
 
+
+st.header("File Download - A Workaround for small data")
+
+text = """\
+    There is currently (20191204) no official way of downloading data from Streamlit. See for
+    example [Issue 400](https://github.com/streamlit/streamlit/issues/400)
+
+    But I discovered a workaround
+    [here](https://github.com/holoviz/panel/issues/839#issuecomment-561538340).
+
+    It's based on the concept of
+    [HTML Data URLs](https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URIs)
+
+    You can try it out below for a dataframe csv file download.
+
+    The methodology can be extended to other file types. For inspiration see
+    [base64.guru](https://base64.guru/converter/encode/file)
+    """
+st.markdown(text)
+
+data = [(1, 2, 3)]
+# When no file name is given, pandas returns the CSV as a string, nice.
+df = pd.DataFrame(data, columns=["Col1", "Col2", "Col3"])
+csv = df.to_csv(index=False)
+b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
+st.markdown(href, unsafe_allow_html=True)
 
