3e676734ddde41379285eaae6faf4cde4b892b42
felipe inostroza <falberto.inostroza@gmail.com>
2021-03-31 22:37:51 -0300

    uploaded

--- app_streamlit.py
+++ app_streamlit.py
@@ -101,17 +101,36 @@ if st.checkbox('Mostrar tasa de incremento de vacunas respecto a una fecha anter
 
         chart_data = get_chart_per_inhabitants( fecha_desde)
 
-        brush = alt.selection(type='interval', encodings=['y'])
+        # brush = alt.selection(type='interval', encodings=['y'])
 
-        st.altair_chart(alt.Chart(chart_data, width = 800, height = 800).mark_bar().encode(
-            x=alt.X('delta_peoplevaccinated_per_100'),
-            y=alt.Y('country', sort ='-x'
-                ),color = "country:N"
-        ).interactive().properties(title="Top 30: Change Rate People Vaccinated per 100 inhabitants").resolve_scale(color='shared'))
+        # st.altair_chart(alt.Chart(chart_data, width = 800, height = 800).mark_bar().encode(
+        #     x=alt.X('delta_peoplevaccinated_per_100'),
+        #     y=alt.Y('country', sort ='-x'
+        #         ),color = "country:N"
+        # ).interactive().properties(title="Top 30: Change Rate People Vaccinated per 100 inhabitants").resolve_scale(color='shared'))
+
+
+        fig = px.bar( chart_data, x = "delta_peoplevaccinated_per_100", y = "country",\
+                     orientation = 'h', title = 'Top 30: People Vaccinated per 100.000 inhabitants', hover_name = 'country',\
+                     template = 'plotly_white', color = 'country',width=900, height=1000)
+
+
+        st.plotly_chart(fig)
+
+
+
 
 
         fig = px.line(get_lines(data), x="date", y="people_vaccinated", color="location", hover_name="location", render_mode="svg")
         st.plotly_chart(fig)
+
+
+
+
+
+
+
+
 else:
 
     fecha_desde = 'no'
@@ -120,41 +139,52 @@ else:
     if st.button('Mostrar gráficos de hoy'):
 
 
-        st.write('Total data in database: ', len(data) )
+        # st.write('Total data in database: ', len(data) )
 
-        st.write('Territories in database: ', len(get_df(data, 'no')) )
+        # st.write('Territories in database: ', len(get_df(data, 'no')) )
 
-        st.subheader('Total Vaccinations')
+        # st.subheader('Total Vaccinations')
 
-        chart_total = get_total_vaccinations()
+        # chart_total = get_total_vaccinations()
 
-        st.altair_chart(alt.Chart(chart_total, width = 800, height = 800).mark_bar().encode(
-            x=alt.X('total_vaccinations'),
-            y=alt.Y('country', sort ='-x'
-                ),color = "country:N"
-        ).interactive().properties(title="Top 30: Total Vaccinations by Country").resolve_scale(color='shared'))
+        # st.altair_chart(alt.Chart(chart_total, width = 800, height = 800).mark_bar().encode(
+        #     x=alt.X('total_vaccinations'),
+        #     y=alt.Y('country', sort ='-x'
+        #         ),color = "country:N"
+        # ).interactive().properties(title="Top 30: Total Vaccinations by Country").resolve_scale(color='shared'))
 
-        st.subheader('Vaccinations per inhabitants')
+        # st.subheader('Vaccinations per inhabitants')
+
+        # chart_data = get_chart_per_inhabitants(fecha_desde)
+
+
+
+        # brush = alt.selection(type='interval', encodings=['y'])
+
+        # st.altair_chart(alt.Chart(chart_data, width = 800, height = 800).mark_bar().encode(
+        #     x=alt.X(variable_graficar),
+        #     y=alt.Y('country', sort ='-x'
+        #         ),color = "country:N"
+        # ).interactive().properties(title="Top 30: People Vaccinated per 100.000 inhabitants").resolve_scale(color='shared'))
+
+
+        df = get_total_vaccinations()
+
+
+        fig = px.bar( df, x = "total_vaccinations", y = "country",\
+                     orientation = 'h', title = 'Top 30: Total Vaccinations by Country', hover_name = 'country',\
+                     template = 'plotly_white', color = 'country',width=900, height=1000)
+
+        st.plotly_chart(fig)
 
         chart_data = get_chart_per_inhabitants(fecha_desde)
 
+        fig = px.bar( chart_data, x = "peoplevaccinated_per_100", y = "country",\
+                     orientation = 'h', title = 'Top 30: People Vaccinated per 100.000 inhabitants', hover_name = 'country',\
+                     template = 'plotly_white', color = 'country',width=900, height=1000)
 
 
-        brush = alt.selection(type='interval', encodings=['y'])
-
-        st.altair_chart(alt.Chart(chart_data, width = 800, height = 800).mark_bar().encode(
-            x=alt.X(variable_graficar),
-            y=alt.Y('country', sort ='-x'
-                ),color = "country:N"
-        ).interactive().properties(title="Top 30: People Vaccinated per 100.000 inhabitants").resolve_scale(color='shared'))
-
-
-
-
-
-
-
-
+        st.plotly_chart(fig)
 
 
 
